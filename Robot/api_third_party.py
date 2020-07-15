d8702563f2a52d8d36ac75495fa33dc96dd90a41
yuqi12319 <18373280066@163.com>
2020-07-15 11:17:28 +0800

    update

---
+++ Robot/api_third_party.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+# Name:api_third_party.py
+# Author:qi.yu
+# Time:2020/7/10 3:26 下午
+from Common.request import Request
+
+
+class ThirdParty:
+
+    def __init__(self):
+        self.request = Request()
+
+    # 获取access_token
+    def access_open_token_api(self, url, data):
+        res = self.request.send_request_method('post', url, data)
+        return res
+
+    # 刷新access_token
+    def refresh_open_token_api(self, url, data):
+        res = self.request.send_request_method('post', url, data)
+        return res
+
+    # 注册公司
+    def regist_company_api(self, url, data):
+        res = self.request.send_request_method('post', url, data)
+        return res
+
+    # 批量导入员工
+    def batch_import_employees(self, url, data):
+        res = self.request.send_request_method('post', url, data)
+        return res
+
+
+if __name__ == '__main__':
+    a = ThirdParty()
+    # ThirdParty().regist_company_api('run_all')
+    a = ThirdParty().regist_company_api('run_all')
+    print(a)
