4ea43164058b796fc3c808ecd9d177e5156b6784
yuqi12319 <18373280066@163.com>
2020-07-16 16:01:59 +0800

    update

---
+++ Robot/__init__.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+# Name:__init__.py.py
+# Author:qi.yu
+# Time:2020/7/6 9:53 上午
---
+++ Robot/api_robot.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+# Name:api_robot.py
+# Author:qi.yu
+# Time:2020/7/6 9:55 上午
+from Common.request import Request
+from Conf.config import *
+
+class Payroll:
+
+    def __init__(self):
+        self.request = Request()
+
+    def get_payrollItem_list_api(self,url,data,headers):
+        res = self.request.get_requests(url=url,data=data,headers=headers)
+        return res
+
+    def get_paygroup_list_api(self,url,data,headers):
+        res = self.request.get_requests(url,data,headers)
+        return res
