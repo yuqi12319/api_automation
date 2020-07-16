d134fdb03f3f0f28f1389e21673fb1b5bf5f6a53
yuqi12319 <18373280066@163.com>
2020-07-16 15:34:57 +0800

    update

--- Common/operation_yaml.py
+++ Common/operation_yaml.py
@@ -12,25 +12,6 @@ from ruamel import yaml
 
 path_ya = str(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))) + '/TestData/Yaml/'
 
-
-# def parse():
-#     pages = {}
-#     for root, dirs, files in os.walk(path_ya):
-#         for name in files:
-#             watch_file_path = os.path.join(root, name)
-#             with open(watch_file_path, 'r') as f:
-#                 page = yaml.safe_load(f)
-#                 pages.update(page)
-#         return pages
-#
-#
-# class GetTestData:
-#     @staticmethod
-#     def get_value(key):
-#         _page_list = {}
-#         pages = parse()
-#         return pages[key]
-
 class YamlHandle:
     def __init__(self):
         self.log = log.MyLog()
