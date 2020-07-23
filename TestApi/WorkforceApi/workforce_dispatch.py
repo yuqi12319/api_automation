9e6040f6983c573ac48ea765f256fc1a067ed4d2
yuqi12319 <18373280066@163.com>
2020-07-23 17:03:07 +0800

    update

--- TestApi/WorkforceApi/workforce_dispatch.py
+++ TestApi/WorkforceApi/workforce_dispatch.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-# Name:workforce_assign.py
+# Name:workforce_dispatch.py
 # Author:qi.yu
 # Time:2020/7/22 2:16 下午
 
@@ -11,14 +11,18 @@ class WorkforceAssign(Const):
     def __init__(self):
         super().__init__()
 
+    # 劳务工派遣接口
+    def dispatch_api(self):
+        pass
+
     # 获取劳务工派遣列表接口
-    def assign_list_api(self, url_path, data):
+    def dispatch_list_api(self, url_path, data):
         url = url_path + data['url']
         res = self.request.send_request_method('post', url=url, params=data['params'], headers=self.headers)
         return res
 
     # 获取劳务工派遣详情接口
-    def assign_detail_api(self, url_path, data):
+    def dispatch_detail_api(self, url_path, data):
         url = url_path + data['url']
         res = self.request.send_request_method('get', url=url, params=data['params'], headers=self.headers)
         return res
