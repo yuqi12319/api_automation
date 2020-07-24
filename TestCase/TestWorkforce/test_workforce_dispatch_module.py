5d2761e65341ae7e5564d98baaa0f5682ffc65c8
yuqi12319 <18373280066@163.com>
2020-07-24 13:59:21 +0800

    update

--- TestCase/TestWorkforce/test_workforce_dispatch_module.py
+++ TestCase/TestWorkforce/test_workforce_dispatch_module.py
@@ -18,27 +18,27 @@ class TestAssign:
         self.url_path = Config().get_conf('test_env', 'test3')
 
     @allure.title("劳务工派遣接口test")
-    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceAssign/dispatch.yaml'))
+    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceDispatch/dispatch.yaml'))
     def test_dispatch(self, data):
         res = WorkforceDispatch().dispatch_api(self.url_path, data)
         Assertions().assert_mode(res, data)
 
     # @pytest.mark.skip
     @allure.title("劳务工派遣列表test")
-    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceAssign/dispatch_list.yaml'))
+    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceDispatch/dispatch_list.yaml'))
     def test_dispatch_list(self, data):
         res = WorkforceDispatch().dispatch_list_api(self.url_path, data)
         Assertions().assert_mode(res, data)
 
     # @pytest.mark.skip
     @allure.title("劳务工派遣详情test")
-    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceAssign/dispatch_detail.yaml'))
+    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceDispatch/dispatch_detail.yaml'))
     def test_dispatch_detail(self, data):
         res = WorkforceDispatch().dispatch_detail_api(self.url_path, data)
         Assertions().assert_mode(res, data)
 
     @allure.title("派遣关联申请test")
-    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceAssign/relevance_apply.yaml'))
+    @pytest.mark.parametrize('data', YamlHandle().read_yaml('Workforce/WorkforceDispatch/relevance_apply.yaml'))
     def test_relevance_apply(self, data):
         res = WorkforceDispatch().relevance_apply_api(self.url_path, data)
         Assertions().assert_mode(res, data)
