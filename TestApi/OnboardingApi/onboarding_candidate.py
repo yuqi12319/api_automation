d585624a3866b9a0d83cb36bcd07896f0d65d74e
Nigo <nigo.su@biposervice.com>
2020-08-05 17:14:22 +0800

    方法名修改

--- TestApi/OnboardingApi/onboarding_candidate.py
+++ TestApi/OnboardingApi/onboarding_candidate.py
@@ -1,6 +1,8 @@
 # @Author: Nigo Su
 # @Time: 2020-08-04-11:37
 # @Description:
+import yaml
+
 from TestApi.consts_api import Const
 
 
@@ -16,6 +18,14 @@ class OnboardingCandidate(Const):
     def post_add_candidate_api(self, url_path, data):
         url = url_path + data['url']
         response = self.request.send_request_method('post', url, json=data['body'], headers=self.headers)
+        # # 新增候选人id存入yaml，走入职场景
+        # res = response.json()
+        # with open(r"D:\python\riesling-apitest\TestData\Yaml\Onboarding\get_candidate_detail.yaml") as f:
+        #     content = yaml.load(f.read(), Loader=yaml.FullLoader)
+        #     content['params']['candidateId'] = '{data}'.format(**res)
+        # with open(r"D:\python\riesling-apitest\TestData\Yaml\Onboarding\get_candidate_detail.yaml", 'w') as ff:
+        #     yaml.dump(content, ff)
+        #     ff.close()
         return response
 
     # 获取人员信息(包含offer和入职资料发送信息)
