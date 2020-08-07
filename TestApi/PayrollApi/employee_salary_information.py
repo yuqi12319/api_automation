d12b08cc6045f4dfc21a168b9386c3150ac932e0
noah <noah.huang@biposervice.com>
2020-08-07 17:46:45 +0800

    新增获取员工薪资信息列表表头接口和员工薪资信息列表接口

--- TestApi/PayrollApi/employee_salary_information.py
+++ TestApi/PayrollApi/employee_salary_information.py
@@ -6,10 +6,21 @@ from TestApi.consts_api import Const
 class EmployeeSalaryInformation(Const):
 
     # 获取员工薪资信息列表表头接口
-    def get_employee_salary_information_api(self, url_path, data):
+    def get_employee_salary_information_list_head_api(self, url_path, data):
         url = url_path + data['url']
         response = self.request.send_request_method('get', url, params=data['params'], headers=self.headers)
         return response
+
+    # 获取员工薪资信息列表接口
+    def get_employee_salary_information_list_api(self, url_path, data):
+        url = url_path + data['url']
+        extra_headers = {"X-Page-Size": "20", "x-current-page": "1"}
+        self.headers["X-Page-Size"] = "20"
+        self.headers["x-current-page"] = "1"
+        response = self.request.send_request_method('post', url, params=data['params'], json=data['body'], headers=self.headers)
+        print(response.headers)
+        return response
+
 
 
 
--- TestCase/TestPayroll/test_employee_salary_information.py
+++ TestCase/TestPayroll/test_employee_salary_information.py
@@ -14,9 +14,16 @@ class TestEmployeeSalaryInformation():
         self.url_path = Config().get_conf('test_env', 'test2')
 
     @allure.title('获取员工薪资信息列表表头')
-    @pytest.mark.parametrize('data',YamlHandle().read_yaml(
-        'Payroll/EmployeeSalaryInformation/employee_salary_information.yaml'))
+    @pytest.mark.parametrize('data', YamlHandle().read_yaml(
+        'Payroll/EmployeeSalaryInformation/employee_salary_information_list_head.yaml'))
     def test_employee_salary_employee_information(self, data):
-        response = EmployeeSalaryInformation().get_employee_salary_information_api(self.url_path, data)
+        response = EmployeeSalaryInformation().get_employee_salary_information_list_head_api(self.url_path, data)
+        Assertions().assert_mode(response, data)
+
+    @allure.title('获取员工薪资信息列表')
+    @pytest.mark.parametrize('data', YamlHandle().read_yaml(
+        'Payroll/EmployeeSalaryInformation/employee_salary_information_list.yaml'))
+    def test_employee_salary_information_list(self, data):
+        response = EmployeeSalaryInformation().get_employee_salary_information_list_api(self.url_path, data)
         Assertions().assert_mode(response, data)
 
