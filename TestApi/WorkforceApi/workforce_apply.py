478014de3bb9e7cb97c80bb339b44e7abb2cf86f
yuqi12319 <18373280066@163.com>
2020-08-10 16:50:28 +0800

    update

--- TestApi/WorkforceApi/workforce_apply.py
+++ TestApi/WorkforceApi/workforce_apply.py
@@ -7,29 +7,29 @@ from TestApi.consts_api import Const
 
 class WorkforceApply(Const):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, env):
+        super().__init__(env)
 
     # 提交用工申请接口
-    def send_apply_api(self, url_path, data):
-        url = url_path + data['url']
+    def send_apply_api(self, data):
+        url = self.url_path + data['url']
         res = self.request.send_request_method(method='post', url=url, json=data['body'], headers=self.headers)
         return res
 
     # 获取用工申请列表接口
-    def apply_list_api(self, url_path, data):
-        url = url_path + data['url']
+    def apply_list_api(self, data):
+        url = self.url_path + data['url']
         res = self.request.send_request_method('post', url=url, params=data['params'], json=data['body'], headers=self.headers)
         return res
 
     # 获取用工申请详情接口
-    def apply_detail_api(self, url_path, data):
-        url = url_path + data['url'] + str(data['application_id'])
+    def apply_detail_api(self, data):
+        url = self.url_path + data['url'] + str(data['application_id'])
         res = self.request.send_request_method('get', url=url, headers=self.headers)
         return res
 
     # 停止申请接口
-    def stop_apply_api(self, url_path, data):
-        url = url_path + data['url'] + str(data['application_id'])
+    def stop_apply_api(self, data):
+        url = self.url_path + data['url'] + str(data['application_id'])
         res = self.request.send_request_method('put', url=url, headers=self.headers)
         return res
--- TestApi/consts_api.py
+++ TestApi/consts_api.py
@@ -4,12 +4,21 @@
 # Time:2020/7/22 3:27 下午
 
 from Common.request import Request
+from Conf.config import Config
 import Common.consts
 
 
 class Const(object):
 
-    def __init__(self):
+    def __init__(self, env):
         self.request = Request()
+        if env == "test1":
+            self.url_path = Config().get_conf('test_env', 'test1')
+        elif env == "test2":
+            self.url_path = Config().get_conf('test_env', 'test2')
+        elif env == "test3":
+            self.url_path = Config().get_conf('test_env', 'test3')
+        else:
+            self.url_path = Config().get_conf('test_env', 'test')
         self.headers = dict()
         self.headers['X-Dk-Token'] = Common.consts.ACCESS_TOKEN[0]
