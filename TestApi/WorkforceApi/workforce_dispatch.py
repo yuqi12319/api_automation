4bbf158a377da4fccd54ffdccf788e73dd8f4c2a
Leo <Leo.Liu@biposervice.com>
2020-08-12 10:56:04 +0800

    Merge branch 'test' of https://gitee.com/bipo/riesling-apitest into test
    # Conflicts:
    #	TestApi/WorkforceApi/workforce_dispatch.py

--- TestApi/WorkforceApi/workforce_dispatch.py
+++ TestApi/WorkforceApi/workforce_dispatch.py
@@ -8,29 +8,29 @@ from TestApi.consts_api import Const
 
 class WorkforceDispatch(Const):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, env):
+        super().__init__(env)
 
     # 劳务工派遣接口
-    def dispatch_api(self, url_path, data):
-        url = url_path + data['url']
+    def dispatch_api(self, data):
+        url = self.url_path + data['url']
         res = self.request.send_request_method('post',url=url, json=data['body'], headers=self.headers)
         return res
 
     # 获取劳务工派遣列表接口
-    def dispatch_list_api(self, url_path, data):
-        url = url_path + data['url']
+    def dispatch_list_api(self, data):
+        url = self.url_path + data['url']
         res = self.request.send_request_method('post', url=url, params=data['params'], headers=self.headers)
         return res
 
     # 获取劳务工派遣详情接口
-    def dispatch_detail_api(self, url_path, data):
-        url = url_path + data['url']
+    def dispatch_detail_api(self, data):
+        url = self.url_path + data['url']
         res = self.request.send_request_method('get', url=url, params=data['params'], headers=self.headers)
         return res
 
     # 关联申请查询
-    def relevance_apply_api(self, url_path, data):
-        url = url_path + data['url']
+    def relevance_apply_api(self, data):
+        url = self.url_path + data['url']
         res = self.request.send_request_method('get', url=url, params=data['params'], headers=self.headers)
         return res
